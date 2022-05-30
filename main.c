7ee7196851561dea4275fb26858001c7472f4538
IRIA <IRIA@LAPTOP-EF9SNPIR>
2022-05-30 19:47:56 +0200

    a

--- main.c
+++ main.c
@@ -20,48 +20,43 @@ ListaEntradas lEntradas;
 #define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 6000
 
-/*
- * MAIN
- */
-
 int main(int argc, char *argv[]) {
-	sqlite3_open("sqlite3/deustoFest.sqlite", &db);
-	pCart = &cart;
-	//menu(db, pCart, lEntradas);
-
-	/*-------------------------- SOCKETS----------------------------------*/
-
 	WSADATA wsaData;
-	SOCKET conn_socket;
-	SOCKET comm_socket;
+	SOCKET conn_socket; //el que lleva la conexion
+	SOCKET comm_socket; //el que lo comunica
 	struct sockaddr_in server;
 	struct sockaddr_in client;
-	char sendBuff[512], recvBuff[512];
-
-	char opcion, conCamping, conBus, opc;
+	char sendBuff[512], recvBuff[512]; // lo que yo envio, lo que yo recibo
+	sqlite3 *db;
+	char opcion, conCamping, conBus;
 	Cartelera cart;
 	int i, precio;
 	char dni[10], nom[20], email[50];
+	sqlite3_open("sqlite3/deustoFest.sqlite", &db);
 	ListaEntradas le;
 
-	printf("\nInitialising Winsock...\n");
+	printf("\nInitialising Winsock...\n"); // inicializa la libreria
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
 		printf("Failed. Error Code : %d", WSAGetLastError());
 		return -1;
 	}
+
 	printf("Initialised.\n");
 
+	//SOCKET creation creacion del socket( la primera estructura
 	if ((conn_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
 		printf("Could not create socket : %d", WSAGetLastError());
 		WSACleanup();
 		return -1;
 	}
-	printf("Socket created.\n");
 
-	server.sin_addr.s_addr = inet_addr(SERVER_IP);
+	printf("Socket created.\n");
+	// cual es la ip y cual es el puerto
+	server.sin_addr.s_addr = inet_addr(SERVER_IP); //INADDR_ANY;
 	server.sin_family = AF_INET;
 	server.sin_port = htons(SERVER_PORT);
 
+	//BIND (the IP/port with socket)
 	if (bind(conn_socket, (struct sockaddr*) &server,
 			sizeof(server)) == SOCKET_ERROR) {
 		printf("Bind failed with error code: %d", WSAGetLastError());
@@ -69,8 +64,10 @@ int main(int argc, char *argv[]) {
 		WSACleanup();
 		return -1;
 	}
-	printf("Bind done.\n");
 
+	printf("Bind done.\n"); //DEJAR EL SOCKET EN ESPERA
+
+	//LISTEN to incoming connections (socket server moves to listening mode)
 	if (listen(conn_socket, 1) == SOCKET_ERROR) {
 		printf("Listen failed with error code: %d", WSAGetLastError());
 		closesocket(conn_socket);
@@ -78,10 +75,11 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 
+	//ACCEPT incoming connections (server keeps waiting for them)
 	printf("Waiting for incoming connections...\n");
 	int stsize = sizeof(struct sockaddr);
 	comm_socket = accept(conn_socket, (struct sockaddr*) &client, &stsize);
-
+	// Using comm_socket is able to send/receive data to/from connected client
 	if (comm_socket == INVALID_SOCKET) {
 		printf("accept failed with error code : %d", WSAGetLastError());
 		closesocket(conn_socket);
@@ -91,13 +89,13 @@ int main(int argc, char *argv[]) {
 	printf("Incomming connection from: %s (%d)\n", inet_ntoa(client.sin_addr),
 			ntohs(client.sin_port));
 
+	// Closing the listening sockets (is not going to be used anymore)
 	closesocket(conn_socket);
 	int fin = 0;
 
 	do {
 		recv(comm_socket, recvBuff, sizeof(recvBuff), 0);
-		sscanf(recvBuff, "%c", &opc);
-
+		sscanf(recvBuff, "%c", &opcion);
 		switch (opcion) {
 		case '1':
 			obtenerCartelera(db, &cart);
@@ -157,12 +155,11 @@ int main(int argc, char *argv[]) {
 		}
 	} while (opcion != '4');
 
+	// CLOSING the sockets and cleaning Winsock...
+	closesocket(comm_socket);
+	WSACleanup();
 
-closesocket( comm_socket);
-WSACleanup();
-
-/*------------------------- END SOCKETS -------------------------------*/
-
-return 0;
+	return 0;
+	//-----------------------------------------------------------
 }
 
